e6dee806239517c1706797ed6c2eb5e99bb91572
RyabovEvgeniy <likerus99@gmail.com>
2022-05-23 21:07:00 +0600

    Закончил верстать section reviews

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-$(function(){
+$(function () {
 	$('.top__slider').slick({
 		arrows: false,
 		dots: true,
 		autoplay: true,
 		fade: true
 	});
-	
 
-
+	$('.reviews__slider').slick({
+		arrows: false,
+		dots: true,
+		slidesToShow: 4,
+		slidesToScroll: 2,
+		autoplay: true,
+		autoplaySpeed: 3000
+	});
 });
