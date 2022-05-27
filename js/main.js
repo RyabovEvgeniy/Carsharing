c4199a32fc6b3f27bef7c8a49bebfc4a5acb4f25
RyabovEvgeniy <likerus99@gmail.com>
2022-05-27 11:43:18 +0600

    Запрет скорола body при открытом меню

--- js/main.js
+++ js/main.js
@@ -19,5 +19,6 @@ $(function () {
 $(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
 	});
 });
